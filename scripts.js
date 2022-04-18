81ca6c5ce8b5bf0805f5175302cdb65b8e314748
Kayck Schuenck <ycferreiras@gmail.com>
2022-04-18 03:05:32 -0300

    falta apenas as mensagens reservadas

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 iniciarSite()
 let nickentrada;
+let destinatario="Todos";
+let tipoMensagem="message";
 function entrarChat(){
     nickentrada={
         name:document.querySelector(".telainicial input").value
@@ -40,7 +42,7 @@ function carregarSite(){
 document.querySelector(".telainicial").classList.add("hidden")
 document.querySelector("header").innerHTML=`
 <img/ src="images/logo.png">
-<div onclick="getParticipantes()"><ion-icon name="people"></ion-icon></div>`
+<div onclick="getParticipantes();montarLista();"><ion-icon name="people"></ion-icon></div>`
 receberMensagem()
 document.querySelector(".barrafinal").innerHTML=`
 <input type="text" placeholder="Escreva aqui..."/>
@@ -50,7 +52,6 @@ document.querySelector(".barrafinal input").addEventListener("keypress", functio
         enviarmensagem();
     }
 })
-
 setInterval(manterServidor,4500)
 setInterval(receberMensagem,2000)
 }
@@ -60,12 +61,11 @@ function manterServidor(){
 }
 
 function enviarmensagem(){
-    alert("aaa")
     const dados={
         from: nickentrada.name,
-        to: "Todos",
+        to: destinatario,
         text: document.querySelector(".barrafinal input").value,
-        type: "message"
+        type: tipoMensagem
     }
     const promise=axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",dados)
     document.querySelector(".barrafinal input").value=""
@@ -87,35 +87,101 @@ function renderizarMensagem(resposta){
     <strong>${resposta.data[i].from}</strong> 
     para <strong>${resposta.data[i].to}</strong>: ${resposta.data[i].text}</h1></div>`
     document.querySelector('.mensagens:last-child').scrollIntoView()
+    }
 }
-}
-function getParticipantes(){
-    const promise=axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
-    document.querySelector("body").innerHTML+=`
-    <div class="filtroPreto" onclick="retornar()"></div>
-    <div class="listaUsuarios"></div>`
-    promise.then(listaParticipantes)
-}
-function listaParticipantes(resposta){
+function montarLista(){
+    document.querySelector(".filtroPreto").classList.remove("hidden")
+    document.querySelector(".listaUsuarios").classList.remove("hidden")
     let elemento=document.querySelector(".listaUsuarios")
     elemento.innerHTML=`        
     <strong>Escolha um contato para enviar mensagem:</strong>
-    <div class="pessoas"><ion-icon name="people""></ion-icon> <p>Todos</p></div>`
-    
+    <div class="feedParticipantes">
+    </div>
+    <strong>Escolha a visibilidade:</strong>
+    <div class="pessoas publico visibilidadeselecionada" onclick="tornarPublico()">
+        <ion-icon name="lock-open"></ion-icon> <p>Público</p>
+        <div class="icone2"><ion-icon name="checkmark"></ion-icon></div>
+    </div>
+    <div class="pessoas reservado" onclick="tornarReservado(this)"><ion-icon name="lock-closed"></ion-icon> <p>Reservadamente</p></div>`
+    setInterval(getParticipantes,10000)
+}
+
+function getParticipantes(){
+    const promise=axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
+    promise.then(renderizarParticipantes)
+}
+function renderizarParticipantes(resposta){
+    let elemento=document.querySelector(".feedParticipantes")
+    elemento.innerHTML=`
+    <div class="pessoas participanteselecionado" onclick="check(this);tornarPublico();">
+    <ion-icon name="people"></ion-icon> <p>Todos</p>
+    </div>`
     for(i=0;i<resposta.data.length;i++){
         elemento.innerHTML+=`
-        <div class="pessoas" onclick="check()"><ion-icon name="person-circle"></ion-icon>
+        <div class="pessoas" onclick="check(this)"><ion-icon name="person-circle"></ion-icon>
         <p>${resposta.data[i].name}</p></div>`
     }
-    elemento.innerHTML+=`
-    <strong>Escolha a visibilidade:</strong>
-    <div class="pessoas" onclick="check()"><ion-icon name="lock-open"></ion-icon> <p>Público</p></div>
-    <div class="pessoas" onclick="check()"><ion-icon name="lock-closed"></ion-icon> <p>Reservadamente</p></div>`
-    //<ion-icon name="checkmark"></ion-icon>
 }
 function retornar(){
-    const el1=document.querySelector(".filtroPreto")
-    const el2=document.querySelector(".listaUsuarios")
-    el1.remove()
-    el2.remove()
+    document.querySelector(".filtroPreto").classList.add("hidden")
+    document.querySelector(".listaUsuarios").classList.add("hidden")
+}
+
+function check(elemento){
+    if(document.querySelector(".participanteselecionado")===null){
+        elemento.classList.add("participanteselecionado")
+    }
+    if(document.querySelector(".icone")===null){
+        elemento.innerHTML+=`<div class="icone"><ion-icon name="checkmark"></ion-icon></div>`
+    }
+    document.querySelector(".participanteselecionado").classList.remove("participanteselecionado")
+    document.querySelector(".icone").remove()
+    elemento.classList.add("participanteselecionado")
+    elemento.innerHTML+=`<div class="icone"><ion-icon name="checkmark"></ion-icon></div>`
+    destinatario=elemento.innerText
+    if(document.querySelector(".visibilidadeselecionada").innerText==="Reservadamente"){
+        tipoMensagem="private_message"
+        document.querySelector(".barrafinal").innerHTML=`<div>
+        <input type="text" placeholder="Escreva aqui..."/> <br>
+        <span>Enviando para ${destinatario} (reservadamente)</span> </div>
+        <div onclick="enviarmensagem()"><ion-icon name="paper-plane-outline"></ion-icon></div>`
+        document.querySelector(".barrafinal input").addEventListener("keypress", function(envio){
+            if(envio.key === "Enter"){
+                enviarmensagem();
+            }
+        })
+    }
+}
+function tornarReservado(){
+        let elemento=document.querySelector(".reservado")
+        tipoMensagem="private_message"
+        document.querySelector(".visibilidadeselecionada").classList.remove("visibilidadeselecionada")
+        document.querySelector(".icone2").remove()
+        elemento.classList.add("visibilidadeselecionada")
+        elemento.innerHTML+=`<div class="icone2"><ion-icon name="checkmark"></ion-icon></div>`
+        document.querySelector(".barrafinal").innerHTML=`<div>
+        <input type="text" placeholder="Escreva aqui..."/> <br>
+        <span>Enviando para ${destinatario} (reservadamente)</span> </div>
+        <div onclick="enviarmensagem()"><ion-icon name="paper-plane-outline"></ion-icon></div>`
+        document.querySelector(".barrafinal input").addEventListener("keypress", function(envio){
+            if(envio.key === "Enter"){
+                enviarmensagem();
+            }
+        })
+}
+function tornarPublico(){
+    let elemento=document.querySelector(".publico")
+    tipoMensagem="message";
+    document.querySelector(".visibilidadeselecionada").classList.remove("visibilidadeselecionada")
+    document.querySelector(".icone2").remove()
+    elemento.classList.add("visibilidadeselecionada")
+    elemento.innerHTML+=`<div class="icone2"><ion-icon name="checkmark"></ion-icon></div>`
+    document.querySelector(".barrafinal").innerHTML=`
+    <input type="text" placeholder="Escreva aqui..."/>
+    <div onclick="enviarmensagem()"><ion-icon name="paper-plane-outline"></ion-icon></div>`
+    document.querySelector(".barrafinal input").addEventListener("keypress", function(envio){
+        if(envio.key === "Enter"){
+            enviarmensagem();
+        }
+    })
 }
